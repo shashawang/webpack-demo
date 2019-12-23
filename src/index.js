f216b0c6048935db6e5555a042353e4c369ccaba
shashawang <15951883863@163.com>
2019-12-23 22:49:13 +0800

    Output Management

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
 import _ from 'lodash';
-import './style.css';
-import masterMiao from './masterMiao.png';
-import Data from './data.xml';
+import printMe from './print.js';
 
 function component() {
     var element = document.createElement('div');
+    var btn = document.createElement('button');
   
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
 
-    var myIcon = new Image();
-    myIcon.src = masterMiao;
-    element.appendChild(myIcon);
-
-    console.log(Data);
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+    element.appendChild(btn);
 
     return element;
   }
