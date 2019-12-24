a413e25920b88f5e79f17916cbc3aa2f62b1c0e4
shashawang <15951883863@163.com>
2019-12-24 23:28:52 +0800

    开发

--- webpack.config.js
+++ webpack.config.js
@@ -7,14 +7,19 @@ module.exports = {
     app: './src/index.js',
     print: './src/print.js'
   },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
   plugins: [
     new CleanWebpackPlugin(), // 官网的这个插件用法可能没更新，会报错不是构造函数，搜了下发现很多人碰到过这个问题
     new HtmlWebpackPlugin({
       title: 'Output Management'
     })
-  ]
+  ],
+  devtool: 'inline-source-map',
+  // devServer: { // 有web-dev-server和下面publicPath web-dev-middlemare的配置，打包出来的就不会生成index.html
+  //   contentBase: './dist'
+  // },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    // publicPath: '/' // web-dev-middlemare处理webpack文件给到server或express的app使用，publicPath会在服务器脚本用到
+  },
 };
