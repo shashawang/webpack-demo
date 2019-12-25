7fff596ba5d6e216f2ff48ce60ee2c097fcec6d0
shashawang <15951883863@163.com>
2019-12-25 19:37:59 +0800

    tree shaking mode

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,5 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  mode: "development"
+  mode: "development" // development模式打包出来的bundle.js不是一行，但也和指南上的不同；production模式打包出来的事一行
 };
