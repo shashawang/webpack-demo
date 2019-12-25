ba64cb0c16751d274c6d614046d0c27d6926ee41
shashawang <15951883863@163.com>
2019-12-25 22:55:58 +0800

    00start

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
-import { cube } from './math.js';
+import _ from 'lodash';
 
 function component() {
-  var element = document.createElement('pre');
+  var element = document.createElement('div');
 
-  element.innerHTML = [
-    'Hello webpack!',
-    '5 cubed is equal to ' + cube(5)
-  ].join('\n\n');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
   return element;
 }
 
 document.body.appendChild(component());
-  
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,9 @@
 const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: {
-    index: './src/index.js',
-    another: './src/another-module.js'
-  },
-  plugins: [
-    new HTMLWebpackPlugin({
-      title: 'Code Splitting'
-    })
-  ],
+  entry: './src/index.js',
   output: {
-    filename: '[name].bundle.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   }
 };
