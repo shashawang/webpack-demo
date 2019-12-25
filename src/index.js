0544e87a9fab79e997e2db687e2871bdf3713eea
shashawang <15951883863@163.com>
2019-12-25 17:43:36 +0800

    tree shaking not success

--- src/index.js
+++ src/index.js
@@ -1,31 +1,15 @@
-import _ from 'lodash';
-import printMe from './print.js';
-import './styles.css';
+import { cube } from './math.js';
 
 function component() {
-    var element = document.createElement('div');
-    var btn = document.createElement('button');
-  
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  var element = document.createElement('pre');
 
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
-    element.appendChild(btn);
+  element.innerHTML = [
+    'Hello webpack!',
+    '5 cubed is equal to ' + cube(5)
+  ].join('\n\n');
 
-    return element;
-  }
-  
-  document.body.appendChild(component());
-  // let element = component(); // 当 print.js 改变导致页面重新渲染时，重新获取渲染的元素
-  // document.body.appendChild(element);
+  return element;
+}
 
-  if (module.hot) {
-    module.hot.accept('./print.js', function(){
-      console.log('Accepting the updated printMe module!');
-      printMe();
-      // document.body.removeChild(element); // TODO:问题这一节没懂啥意思，修改前后效果一样
-      // element = component(); // 重新渲染页面后，component 更新 click 事件处理
-      // document.body.appendChild(element);
-    })
-  }
+document.body.appendChild(component());
   
