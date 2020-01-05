33323007a39ab88e6144ee77497f095001bd4854
shashawang <15951883863@163.com>
2020-01-05 23:17:31 +0800

    debug

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-import _ from 'lodash';
+// import _ from 'lodash';
+import { file, parse } from './globals.js';
 
 function component() {
   var element = document.createElement('div');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = join(['Hello', 'webpack'], ' ');
+
+  // Assume we are in the context of `window`
+  this.alert('Hmmm, this probably isn\'t a great idea...')
+  // this.alert(file)
+  // parse()
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,33 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
+  // entry: {
+  //   polyfills: './src/polyfills.js',
+  //   index: './src/index.js'
+  // },
   output: {
     filename: 'bundle.js',
+    // filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  module: {
+    rules: [
+      // {
+      //   test: require.resolve('./src/index.js'),
+      //   use: 'imports-loader?this=>window' // 将CommonJS 环境下的 this(module.exports)用 imports-loader 覆写 
+      // },
+      // {
+      //   test: require.resolve('./src/globals.js'), // 文档上的路径会报错
+      //   use: 'exports-loader?file,parse=helpers.parse'
+      // }
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      // _: 'lodash',
+      join: ['lodash', 'join']
+    })
+  ]
 };
