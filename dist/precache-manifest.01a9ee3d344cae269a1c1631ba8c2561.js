74445158cb970c037c7f19e139c0c69586bd8618
shashawang <15951883863@163.com>
2020-01-06 21:33:27 +0800

    use http-server& workbox-webpack-plugin &register on load

---
+++ dist/precache-manifest.01a9ee3d344cae269a1c1631ba8c2561.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "606d3cb0fd9abc4f1e46",
+    "url": "app.bundle.js"
+  },
+  {
+    "revision": "452dda37fe12ec2353c8597968bf326c",
+    "url": "index.html"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -13,3 +13,12 @@ function component() {
 
 document.body.appendChild(component());
   
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+      console.log('SW registered: ', registration);
+    }).catch(registrationError => {
+      console.log('SW registration failed: ', registrationError);
+    });
+  });
+}
