2935d6914ad2c5bdfa3e4f7ac86a279f079f0c21
shashawang <15951883863@163.com>
2020-01-06 20:59:41 +0800

    use whatwg-fetch & babel-preset-env useBuiltIns option & node & script-loader & noParse

--- src/index.js
+++ src/index.js
@@ -10,9 +10,17 @@ function component() {
   // Assume we are in the context of `window`
   // this.alert('Hmmm, this probably isn\'t a great idea...')
   // this.alert(file)
-  parse()
+  // parse()
 
   return element;
 }
 
 document.body.appendChild(component());
+
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(json => {
+    console.log('We retrieved some data! AND we\'re confident it will work on a variety of browser distributions.')
+    console.log(json)
+  })
+  .catch(error => console.error('Something went wrong when fetching this data: ', error))
--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,14 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-  entry: './src/index.js',
-  // entry: {
-  //   polyfills: './src/polyfills.js',
-  //   index: './src/index.js'
-  // },
+  // entry: './src/index.js',
+  entry: {
+    polyfills: './src/polyfills.js',
+    index: './src/index.js'
+  },
   output: {
-    filename: 'bundle.js',
-    // filename: '[name].bundle.js',
+    // filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   module: {
